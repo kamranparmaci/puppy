34c5417b74641f1a6df839fc1663521f553dbdaf
kamran <kamranparmasii@gmail.com>
2024-07-15 00:59:17 +0330

    add dog package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kamranparmaci/puppy
 
 go 1.22.5
+
+require github.com/kamranparmaci/dog v0.0.0-20240714212709-c0477b938354 // indirect
--- puppy.go
+++ puppy.go
@@ -1,8 +1,15 @@
 package puppy
 
+import (
+	"github.com/kamranparmaci/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 func Barks() string {
 	return "woof! woof! woof!"
+}
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
